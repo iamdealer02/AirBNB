27cb8517484b122ad643157d91011be08b24fbc6
iamdealer02 <sharmaupasana823@gmail.com>
2024-01-03 12:57:39 +0100

    Updates

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Homepage from "./views/Homepage";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Homepage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
